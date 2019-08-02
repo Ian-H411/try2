4ce50b750c09ab54cd3f1ed37c40fe7540340608
Ian_H411 <ian.h411@gmail.com>
2019-08-02 11:28:41 -0600

    Initial Commit

---
+++ try2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  try2
+//
+//  Created by Ian Hall on 8/2/19.
+//  Copyright © 2019 Ian Hall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
